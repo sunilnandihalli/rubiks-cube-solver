7ece96e6b80b6fcfc645a1ba574d887b9ad33acc
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2016-03-06 22:17:48 +0530

    fixed some issues with project.clj

--- project.clj
+++ project.clj
@@ -12,18 +12,19 @@
                  [compojure "1.4.0"]
                  [cheshire "5.5.0"]
                  ;; CLJS
-                 [org.clojure/clojurescript "1.7.228"]
-                 [org.clojure/core.async "0.2.374"]
-                 [cljs-http "0.1.39"]
+                 [org.clojure/clojurescript "1.7.228" :exclusions [org.clojure/tools.reader]]
+                 [org.clojure/core.async "0.2.374" :exclusions [org.clojure/tools.reader]]
+                 [cljs-http "0.1.39" :exclusions [commons-codec org.clojure/tools.reader]]
                  [reagent "0.5.1"]
                  [prismatic/dommy "1.1.0"]]
 
   :plugins [[lein-cljsbuild "1.1.2"]
             [lein-ring "0.9.7"]
             [lein-pdo "0.1.1"]
-            [lein-gorilla "0.3.6"]]
-
-
+          ;  [lein-gorilla "0.3.6"]
+            ]
+  :hooks [leiningen.cljsbuild]
+  :aot :all
 
   :aliases {"dev" ["pdo" "cljsbuild" "auto" "dev," "ring" "server-headless"]}
 
@@ -32,10 +33,39 @@
 
   :source-paths ["src/clj" "src/cljc"]
 
-  :cljsbuild {:builds [{:id "dev"
-                        :source-paths ["src/cljs" "src/cljc"]
-                        :compiler {:output-to "resources/public/js/rubiks_cloact_webapp.js"
-                                   :output-dir "resources/public/js/out"
-                                   :optimizations :none
-                                   :source-map true
-                                   :externs ["js/scenejs/scenejs.js"]}}]})
+  :cljsbuild {:builds
+              {:dev
+               {:id "dev"
+                :source-paths ["src/cljs" "src/cljc"]
+                :compiler {:output-to "resources/public/js/rubiks_cloact_webapp.js"
+                           :output-dir "resources/public/js/out/"
+                           :optimizations :whitespace
+                           :verbose true
+                           :warnings true
+                           :closure-warnings {:access-controls :warning
+                                              :ambiguous-function-decl :warning
+                                              :debugger-statement-present :warning
+                                              :check-regexp :warning
+                                              :check-types :warning
+                                              :check-useless-code :warning
+                                              :check-variables :warning
+                                              :const :warning
+                                              :constant-property :warning
+                                              :deprecated :warning
+                                              :duplicate-message :warning
+                                              :es5-strict :warning
+                                              :externs-validation :warning
+                                              :fileoverview-jsdoc :warning
+                                              :global-this :warning
+                                              :internet-explorer-checks :warning
+                                              :invalid-casts :warning
+                                              :missing-properties :warning
+                                              :non-standard-jsdoc :warning
+                                              :strict-module-dep-check :warning
+                                              :tweaks :warning
+                                              :undefined-names :warning
+                                              :undefined-variables :warning
+                                              :unknown-defines :warning
+                                              :visiblity :warning}
+
+                           :externs ["js/scenejs/scenejs.js"]}}}})
