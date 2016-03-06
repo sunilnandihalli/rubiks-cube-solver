827df811c00cbc9362fb83ca2193c3ad42d70712
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2016-03-06 16:45:48 +0530

    trying to port the old rubiks cube to newer libraries

--- project.clj
+++ project.clj
@@ -2,46 +2,38 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.reader "0.8.3"]
-                 [org.clojure/math.combinatorics "0.0.7"]
-                 [org.clojure/core.logic "0.8.7"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.reader "0.10.0"]
+                 [org.clojure/math.combinatorics "0.1.1"]
+                 [org.clojure/core.logic "0.8.10"]
                  ;; CLJ
-                 [ring/ring-core "1.2.2"]
-                 [ring/ring-json "0.3.0"]
-                 [compojure "1.1.6"]
-                 [cheshire "5.3.1"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-json "0.4.0"]
+                 [compojure "1.4.0"]
+                 [cheshire "5.5.0"]
                  ;; CLJS
-                 [org.clojure/clojurescript "0.0-2199"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [cljs-http "0.1.9"]
-                 [reagent "0.4.2"]
-                 [prismatic/dommy "0.1.2"]]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [org.clojure/core.async "0.2.374"]
+                 [cljs-http "0.1.39"]
+                 [reagent "0.5.1"]
+                 [prismatic/dommy "1.1.0"]]
 
-  :plugins [[lein-cljsbuild "1.0.3"]
-            [lein-ring "0.8.7"]
+  :plugins [[lein-cljsbuild "1.1.2"]
+            [lein-ring "0.9.7"]
             [lein-pdo "0.1.1"]
-            [lein-gorilla "0.1.2"]
-            [com.keminglabs/cljx "0.3.2"]]
+            [lein-gorilla "0.3.6"]]
 
-  :cljx {:builds [{:source-paths ["src/cljx"]
-                   :output-path "target/generated/clj"
-                   :rules :clj}
 
-                  {:source-paths ["src/cljx"]
-                   :output-path "target/generated/cljs"
-                   :rules :cljs}]}
 
-  :hooks [cljx.hooks]
   :aliases {"dev" ["pdo" "cljsbuild" "auto" "dev," "ring" "server-headless"]}
 
   :ring {:handler rubiks-cloact-webapp.core/app
          :init    rubiks-cloact-webapp.core/init}
 
-  :source-paths ["src/clj" "target/generated/clj"]
+  :source-paths ["src/clj" "src/cljc"]
 
   :cljsbuild {:builds [{:id "dev"
-                        :source-paths ["src/cljs" "target/generated/cljs"]
+                        :source-paths ["src/cljs" "src/cljc"]
                         :compiler {:output-to "resources/public/js/rubiks_cloact_webapp.js"
                                    :output-dir "resources/public/js/out"
                                    :optimizations :none
